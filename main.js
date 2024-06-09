7a57304956839f9d72b457dcd04887609d5d6ee4
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-09 23:54:41 +0530

    Add code that generates a 16x16 grid of squares

---
+++ main.js
@@ -0,0 +1,13 @@
+const gridContainer = document.querySelector(".grid-container");
+
+for(let i = 0; i < 16; i++) {
+    const squareRow = document.createElement("div");
+    squareRow.classList.add("row");
+    gridContainer.appendChild(squareRow);
+
+    for(let j = 0; j < 16; j++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        squareRow.appendChild(squareDiv);
+    }
+}
