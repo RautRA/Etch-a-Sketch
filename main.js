40a50c327e67262b334fd4c05b0cf2bc56664e50
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-10 20:29:23 +0530

    Add functionality to let users specify grid size

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 const gridContainer = document.querySelector(".grid-container");
+const gridSizeBtn = document.querySelector(".grid-size-btn");
 
-for(let i = 0; i < 16; i++) {
-    const squareRow = document.createElement("div");
-    squareRow.classList.add("row");
-    gridContainer.appendChild(squareRow);
-
-    for(let j = 0; j < 16; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("square");
-        squareRow.appendChild(squareDiv);
+function generateGrid(gridSize) {
+    gridContainer.innerHTML = "";
+    
+    for(let i = 0; i < gridSize; i++) {
+        const squareRow = document.createElement("div");
+        squareRow.classList.add("row");
+        gridContainer.appendChild(squareRow);
+        
+        for(let j = 0; j < gridSize; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("square");
+            squareRow.appendChild(squareDiv);
+        }
     }
+
+    const squareDivs = document.querySelectorAll(".square");
+    
+    squareDivs.forEach(squareDiv => {
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "black";
+        });
+    });
 }
 
-const squareDivs = document.querySelectorAll(".square");
-
-squareDivs.forEach(squareDiv => {
-    squareDiv.addEventListener("mouseover", () => {
-        squareDiv.style.backgroundColor = "black";
-    });
+gridSizeBtn.addEventListener("click", () => {
+    const gridSize = Number(prompt("Enter the dimension for the new grid between 2 to 100: ", 16));
+    if (!gridSize) {
+        alert("Please specity a number between 2 to 100")
+    } else if (gridSize > 100 || gridSize < 2) {
+        alert("Sorry you can only specity a number between 2 to 100")
+    } else {
+        generateGrid(gridSize);
+    }
 });
+
+generateGrid(16);
